082d2414e71440db4e990bdcf317256878c95def
Santiago <baezsantiago@myocu.oak.edu>
2026-03-29 22:36:41 -0500

    Last commit

--- js/script.js
+++ js/script.js
@@ -1,27 +1,25 @@
 const gifContainer = document.querySelector("#gif-container");
 const fetchBtn = document.querySelector("#fetch-gif-btn");
 const searchInput = document.querySelector("#search-input");
-
 const myKey = "mIsfeDvpmNbAsYkTfCo4dLEtBlrgztAC";
 
 fetchBtn.addEventListener("click", async () => {
     const searchTerm = searchInput.value;
-    
-    // Construir la URL con el término de búsqueda
+    if (!searchTerm) return;
+
     const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${myKey}&q=${searchTerm}&limit=12`;
 
     const response = await fetch(endpoint);
     const result = await response.json();
-    
-    // Limpiar lo que había antes
+    const images = result.data;
+
     gifContainer.innerHTML = "";
 
-    // Dibujar los nuevos GIFs
-    result.data.forEach(gif => {
+    images.forEach(gif => {
         const url = gif.images.fixed_height.url;
         gifContainer.innerHTML += `
             <div class="col-3 mb-3">
-                <img src="${url}" class="img-fluid rounded">
+                <img src="${url}" class="img-fluid rounded" style="height:200px; width:100%; object-fit:cover;">
             </div>`;
     });
 });
